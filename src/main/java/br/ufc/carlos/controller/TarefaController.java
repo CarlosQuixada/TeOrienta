ab06d4174dffcb77e94ce50832e43968fc4bdab4
CarlosQuixada <carlosquixada95@gmail.com>
2017-04-01 18:08:26 -0300

    Visualizando Tarefas fix #10

--- src/main/java/br/ufc/carlos/controller/TarefaController.java
+++ src/main/java/br/ufc/carlos/controller/TarefaController.java
@@ -1,9 +1,12 @@
 package br.ufc.carlos.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.ufc.carlos.model.Tarefa;
@@ -26,5 +29,13 @@ public class TarefaController {
 		tarefa.setUsuario(usuario);
 		tarefaService.addOrUpdateUsuario(tarefa);
 		return "home";
+	}
+	
+	@RequestMapping(value="/listarTarefa")
+	public String listarTarefa(HttpSession session,Model model){
+		Usuario usuario = (Usuario) session.getAttribute("usuario_logado");
+		List<Tarefa> tarefas = usuario.getTarefas();
+		model.addAttribute("tarefas", tarefas);
+		return "tarefa/listarTarefa";
 	}
 }

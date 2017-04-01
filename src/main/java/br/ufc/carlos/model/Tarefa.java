defe561cdb4c0efda15e3b8715c788a01d468306
CarlosQuixada <carlosquixada95@gmail.com>
2017-04-01 17:52:13 -0300

    Cadastrando Tarefa fix #8

---
+++ src/main/java/br/ufc/carlos/model/Tarefa.java
@@ -0,0 +1,53 @@
+package br.ufc.carlos.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Tarefa {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer idTarefa;
+	private String titulo;
+	private String descricao;
+
+	@ManyToOne
+	@JoinColumn(name = "usuario_id", referencedColumnName = "USU_ID")
+	private Usuario usuario;
+
+	public Integer getIdTarefa() {
+		return idTarefa;
+	}
+
+	public void setIdTarefa(Integer idTarefa) {
+		this.idTarefa = idTarefa;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+}
--- src/main/java/br/ufc/carlos/model/Usuario.java
+++ src/main/java/br/ufc/carlos/model/Usuario.java
@@ -1,50 +1,67 @@
 package br.ufc.carlos.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Usuario {
 	@Id
-	@Column(name="USU_ID",nullable=false)
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "USU_ID", nullable = false)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer idUsuario;
 	private String nome;
 	private String email;
 	private String senha;
-		
+
+	@OneToMany(mappedBy = "usuario", targetEntity = Tarefa.class, fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+	private List<Tarefa> tarefas;
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-		
+
 	public Integer getIdUsuario() {
 		return idUsuario;
 	}
-	
+
 	public void setIdUsuario(Integer idUsuario) {
 		this.idUsuario = idUsuario;
 	}
-	
+
 	public String getSenha() {
 		return senha;
 	}
-	
+
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public List<Tarefa> getTarefas() {
+		return tarefas;
+	}
+
+	public void setTarefas(List<Tarefa> tarefas) {
+		this.tarefas = tarefas;
 	}
 
 	@Override
